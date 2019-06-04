7757a0ef7afef274d0024c067cc8f04327f6915c
finchparker <31844879+finchparker@users.noreply.github.com>
2019-06-04 14:34:49 +0900

    Update blockchain.ts

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,17 +1,19 @@
 import * as CryptoJS from 'crypto-js';
 import {broadcastLatest} from './p2p';
+// 새롭게 import 되었다. 
 import {hexToBinary} from './util';
-
+// Block 의 class 를 설정한다. 
 class Block {
 
+    // 블록 구조의 필수적인 요소들에 대한 구현이다. 
     public index: number;
     public hash: string;
     public previousHash: string;
     public timestamp: number;
     public data: string;
+    // 새롭게 추가되었다. 새롭게 block 의 요소로 추가되었다. 
     public difficulty: number;
     public nonce: number;
-
     constructor(index: number, hash: string, previousHash: string,
                 timestamp: number, data: string, difficulty: number, nonce: number) {
         this.index = index;
@@ -19,27 +21,29 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash;
+        // 똑같이 constructor 에도 추가된다. 
         this.difficulty = difficulty;
         this.nonce = nonce;
     }
 }
-
+// genesisBlock 하드코딩 되어있다.
 const genesisBlock: Block = new Block(
+    // 위의 블록구조 처럼 index, hash, previousHash, timestamp, data, difficulty, nonce 순으로 정보가 기입되어있음을 알 수 있다. 
     0, '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627', '', 1465154705, 'my genesis block!!', 0, 0
 );
-
+// 제네시스 블록을 가장 먼저 받아온다. 블록체인 저장을 시작하는 과정이다. 
 let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
-
+// 마지막 블록의 정보를 가지고 오는 과정이다. 현 체인의 길이에서 - 1 을 한 index 를 가지고 있는 블록의 정보를 가지고 온다.
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-// in seconds
+// 블록 생성 주기를 설정해준다. in seconds
 const BLOCK_GENERATION_INTERVAL: number = 10;
 
-// in blocks
+// 난이도 조정 주기를 설정해준다. in blocks
 const DIFFICULTY_ADJUSTMENT_INTERVAL: number = 10;
-
+// 새롭게 추가되었다. 
 const getDifficulty = (aBlockchain: Block[]): number => {
     const latestBlock: Block = aBlockchain[blockchain.length - 1];
     if (latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && latestBlock.index !== 0) {
@@ -48,7 +52,7 @@ const getDifficulty = (aBlockchain: Block[]): number => {
         return latestBlock.difficulty;
     }
 };
-
+// 새롭게 추가되었다. 
 const getAdjustedDifficulty = (latestBlock: Block, aBlockchain: Block[]) => {
     const prevAdjustmentBlock: Block = aBlockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
     const timeExpected: number = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
@@ -63,19 +67,19 @@ const getAdjustedDifficulty = (latestBlock: Block, aBlockchain: Block[]) => {
 };
 
 const getCurrentTimestamp = (): number => Math.round(new Date().getTime() / 1000);
-
+// 블록을 생성하는 과정이다. 
 const generateNextBlock = (blockData: string) => {
-    const previousBlock: Block = getLatestBlock();
+    const previousBlock: Block = getLatestBlock();              // 새로운 블록을 만들 때 그 전 블록으로 현 체인의 마지막 블록을 설정한다. 
     const difficulty: number = getDifficulty(getBlockchain());
     console.log('difficulty: ' + difficulty);
-    const nextIndex: number = previousBlock.index + 1;
-    const nextTimestamp: number = getCurrentTimestamp();
+    const nextIndex: number = previousBlock.index + 1;          // index 를 설정하는 과정이다.
+    const nextTimestamp: number = getCurrentTimestamp();        // #1 에서 약간 더 발전했다. 
     const newBlock: Block = findBlock(nextIndex, previousBlock.hash, nextTimestamp, blockData, difficulty);
     addBlock(newBlock);
-    broadcastLatest();
+    broadcastLatest();      // import 한 {broadcastLatest}이 사용되었다.
     return newBlock;
 };
-
+// 블록을 생성하는 과정이다. 
 const findBlock = (index: number, previousHash: string, timestamp: number, data: string, difficulty: number): Block => {
     let nonce = 0;
     while (true) {
@@ -89,25 +93,27 @@ const findBlock = (index: number, previousHash: string, timestamp: number, data:
 
 const calculateHashForBlock = (block: Block): string =>
     calculateHash(block.index, block.previousHash, block.timestamp, block.data, block.difficulty, block.nonce);
-
+// class Block 의 요소들로 hash 값을 계산하는 과정이다. 
 const calculateHash = (index: number, previousHash: string, timestamp: number, data: string,
                        difficulty: number, nonce: number): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data + difficulty + nonce).toString();
-
+// 새로운 블록을 더하는 과정이다.
 const addBlock = (newBlock: Block) => {
+    // 새롭게 추가될 블록이 유효한 것인지를 확인하는 과정이다. 
     if (isValidNewBlock(newBlock, getLatestBlock())) {
         blockchain.push(newBlock);
     }
 };
-
+// 블록 구조의 유효성을 판단하는 과정이다. 
 const isValidBlockStructure = (block: Block): boolean => {
+    // class Block 에서 정의된 요소들의 각 항과 일치한지 검사한다. 
     return typeof block.index === 'number'
         && typeof block.hash === 'string'
         && typeof block.previousHash === 'string'
         && typeof block.timestamp === 'number'
         && typeof block.data === 'string';
 };
-
+// 새로운 블록의 유효성을 판단하는 과정이다. 
 const isValidNewBlock = (newBlock: Block, previousBlock: Block): boolean => {
     if (!isValidBlockStructure(newBlock)) {
         console.log('invalid structure');
@@ -127,19 +133,19 @@ const isValidNewBlock = (newBlock: Block, previousBlock: Block): boolean => {
     }
     return true;
 };
-
+// 새롭게 추가되었다. 
 const getAccumulatedDifficulty = (aBlockchain: Block[]): number => {
     return aBlockchain
         .map((block) => block.difficulty)
         .map((difficulty) => Math.pow(2, difficulty))
         .reduce((a, b) => a + b);
 };
-
+// 새롭게 추가되었다. 
 const isValidTimestamp = (newBlock: Block, previousBlock: Block): boolean => {
     return ( previousBlock.timestamp - 60 < newBlock.timestamp )
         && newBlock.timestamp - 60 < getCurrentTimestamp();
 };
-
+// 새롭게 추가되었다. 
 const hasValidHash = (block: Block): boolean => {
 
     if (!hashMatchesBlockContent(block)) {
@@ -152,27 +158,27 @@ const hasValidHash = (block: Block): boolean => {
     }
     return true;
 };
-
+// 새롭게 추가되었다. 
 const hashMatchesBlockContent = (block: Block): boolean => {
     const blockHash: string = calculateHashForBlock(block);
     return blockHash === block.hash;
 };
-
+// 새롭게 추가되었다. 
 const hashMatchesDifficulty = (hash: string, difficulty: number): boolean => {
     const hashInBinary: string = hexToBinary(hash);
     const requiredPrefix: string = '0'.repeat(difficulty);
     return hashInBinary.startsWith(requiredPrefix);
 };
-
+// 체인의 유효성을 판단하는 과정이다.
 const isValidChain = (blockchainToValidate: Block[]): boolean => {
+    // 체인의 첫 번째 블록이 genesisBlock 과 일치하는지 확인한다. 
     const isValidGenesis = (block: Block): boolean => {
         return JSON.stringify(block) === JSON.stringify(genesisBlock);
     };
-
     if (!isValidGenesis(blockchainToValidate[0])) {
         return false;
     }
-
+    // isValidNewBlock 을 통하여 전체 체인을 검증한다. 
     for (let i = 1; i < blockchainToValidate.length; i++) {
         if (!isValidNewBlock(blockchainToValidate[i], blockchainToValidate[i - 1])) {
             return false;
@@ -188,15 +194,17 @@ const addBlockToChain = (newBlock: Block) => {
     }
     return false;
 };
-
+// 가장 긴 체인이 유효한 체인으로 교체되는 과정이다.
 const replaceChain = (newBlocks: Block[]) => {
-
+    // 새로운 chain 이 유효한 chain 이고 그 chain 이 기존의 것보다 더 길면 교체된다.
     if (isValidChain(newBlocks) &&
         getAccumulatedDifficulty(newBlocks) > getAccumulatedDifficulty(getBlockchain())) {
         console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
         blockchain = newBlocks;
         broadcastLatest();
-    } else {
+    }
+    // 위의 2가지 조건 중 1개라도 만족하지 못하면 교체되지 않는다.
+    else {
         console.log('Received blockchain invalid');
     }
 };
